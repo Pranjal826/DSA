137a59e88e9fed681c99b201f3c9110d4704bd9e
edit <pranjalshukla245@gmail.com>
2023-10-14 15:23:39 +0530

    cpp

---
+++ class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+// class info{
+// private:
+// int weight;
+// public:
+// int age;
+// void getweight(){
+//     cout<<weight;
+// }
+
+// int setweight(int w){
+//     weight=w;
+//     return w;
+// }
+// };
+// int main(){
+//     info obj;
+//     obj.age=20;
+//     int weight;
+//     cin>>weight;
+//     obj.setweight(weight);
+//     cout<<obj.age<<endl;
+//     obj.getweight();
+//     return 0;
+// }
+
+ 
